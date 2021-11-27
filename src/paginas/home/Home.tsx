f4eb19d9e262eb53f8c32081ca9f19137ff03453
Wesleykfg <wesleykelvin59@gmail.com>
2021-11-26 22:38:50 -0300

    adicionando projeto blog pessoal react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://pet.totalneem.com/wp-content/uploads/2020/06/Hysky_simon-rae-jY_2XG-6HU0-unsplash-002-scaled.jpg" 
+            alt="Imagem Tela Inicial" className="img" />;
+        </>
+    );
+}
+
+export default Home;
