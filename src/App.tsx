814fef65a3e1924a40c0f0a5fa699a9b87d2db4e
Wesleykfg <wesleykelvin59@gmail.com>
2021-12-07 18:04:54 -0300

    Adicionando Temas e postagens alem de refaturar a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,47 @@
 import React from 'react';
 import Navbar from "./components/estaticos/navbar/Navbar";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/estaticos/footer/Footer";
 import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario"
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
 import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <div style={{minHeight: "100vh"}}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <div style={{ minHeight: "100vh" }}>
 
-        <Route exact path="/">
-            <Login/>
+            <Route exact path="/">
+              <Login />
             </Route>
 
-        <Route path="/login">
-            <Login/>
+            <Route path="/login">
+              <Login />
             </Route>
 
-          <Route path="/home">
-            <Home/>
-          </Route>
-          <Route path="/cadastrousuario">
-            <CadastroUsuario/>
+            <Route path="/home">
+              <Home />
             </Route>
-        </div>
-      </Switch>
-      <Footer/>
-    </Router>
+            <Route path="/cadastrousuario">
+              <CadastroUsuario />
+            </Route>
+            <Route path="/temas">
+              <ListaTema />
+            </Route>
+            <Route path="/posts">
+              <ListaPostagem />
+            </Route>
+          </div>
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
--- src/components/services/Service.ts
+++ src/components/services/Service.ts
@@ -6,11 +6,16 @@ export const api = axios.create({
 
 export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
-    setDado(resposta.data.token)
+    setDado(resposta.data)
 }
 
 export const login = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
+} //usuarios/logare
+
+export const buscar = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
 } //usuarios/logar
 
